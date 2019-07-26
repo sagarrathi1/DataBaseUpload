eeb1a3bb5efdc871de61f5d5022f138b932d3b91
Sagar <rathisagar8080@gmail.com>
2019-07-26 16:28:06 +0530

    Button

---
+++ app/src/main/java/com/lakhan/databaseupload/Main2Activity.java
@@ -0,0 +1,39 @@
+package com.lakhan.databaseupload;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+
+    Button upload,orders;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        upload = (Button) findViewById(R.id.UploadButton);
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        orders = (Button)findViewById(R.id.orderButton);
+        orders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
+}
